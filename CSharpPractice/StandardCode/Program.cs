f3a6e6a927d243ed4a7e8342e1eb3485a34ba626
YougA-pi-URA <n-youge.1415926535897932384626@ezweb.ne.jp>
2019-03-12 22:36:07 +0900

    Read & Write string!

--- CSharpPractice/StandardCode/Program.cs
+++ CSharpPractice/StandardCode/Program.cs
@@ -10,7 +10,16 @@ namespace StandardCode
     {
         static void Main(string[] args)
         {
+            string cmd = "";
+            do
+            {
+                cmd = Console.ReadLine();
+                Console.WriteLine(cmd);
+            } while (cmd != "exit");
             
+            //　コンソールが自動で閉じないための処理
+            Console.WriteLine("何かキーを押すと終了します。");
+            Console.ReadKey();
         }
     }
 }
